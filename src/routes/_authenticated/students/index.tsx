21a586266d9b4fb465e487d481aa9b3ab9f1fa43
azineddine <amarichezineddine@gmail.com>
2025-12-20 21:56:17 +0100

    feat: Add authenticated routes for students and updates.

---
+++ src/routes/_authenticated/students/index.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router'
+import StudentsPage from '@/pages/StudentsPage'
+
+export const Route = createFileRoute('/_authenticated/students/')({
+  component: StudentsPage,
+})
