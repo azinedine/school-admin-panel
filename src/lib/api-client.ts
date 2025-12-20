0e1d3f1ed33f807525e1e812e032620fcb557941
azineddine <amarichezineddine@gmail.com>
2025-12-20 14:32:50 +0100

    refactor: Separate AxiosError and AxiosInstance into type-only imports.

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosError, AxiosInstance } from 'axios'
+import axios from 'axios'
+import type { AxiosError, AxiosInstance } from 'axios'
 
 /**
  * Base API URL - configure this based on your environment
