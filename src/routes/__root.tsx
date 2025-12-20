d1402e7cd8015da14c2d7d909185ca0ef898a0ec
azineddine <amarichezineddine@gmail.com>
2025-12-20 21:54:27 +0100

    feat: Add initial TanStack Router setup including a root route, authenticated layout, and multiple authenticated sub-routes.

---
+++ src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { Toaster } from '@/components/ui/sonner'
+
+export const Route = createRootRoute({
+  component: () => {
+    return (
+      <>
+        <Outlet />
+        <Toaster />
+      </>
+    )
+  },
+})
