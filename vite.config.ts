01e9ae7aa48d5c534324d93128e81ea8a0e9d27f
azineddine <amarichezineddine@gmail.com>
2025-12-20 21:47:50 +0100

    feat: Implement authenticated layout and routing with `@tanstack/react-router` and refactor the sidebar to be data-driven.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwind from '@tailwindcss/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tailwind()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
