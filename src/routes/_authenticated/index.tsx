2a91d401464d04df0d53041449130f393dd147bc
azineddine <amarichezineddine@gmail.com>
2025-12-22 22:41:06 +0100

    fix home page

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,6 +1,18 @@
+import { ContentPage } from '@/components/layout/content-page'
 import { createFileRoute } from '@tanstack/react-router'
-import ExamsPage from '@/pages/ExamsPage'
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: ExamsPage,
+  component: HomePage,
 })
+
+function HomePage() {
+  return (
+    <ContentPage title="Home Page" description="Over View">
+      <div className="flex items-center justify-center min-h-[40vh]">
+        <div className="text-center space-y-4">
+          <p className="text-muted-foreground">This page is coming soon...</p>
+        </div>
+      </div>
+    </ContentPage>
+  )
+}
