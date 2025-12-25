335889cbe411138adda91264fdceb87c79fcb62c
azineddine <amarichezineddine@gmail.com>
2025-12-25 10:11:15 +0100

    Implement authentication check in route: Add beforeLoad hook to redirect unauthenticated users to the login page, enhancing security and user flow in the application.

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { AuthenticatedLayout } from '@/components/layout/authenticated-layout'
+import { useAuthStore } from '@/store/auth-store'
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: ({ location }) => {
+    if (!useAuthStore.getState().isAuthenticated) {
+      throw redirect({
+        to: '/login',
+        search: {
+          redirect: location.href,
+        },
+      })
+    }
+  },
   component: AuthenticatedLayout,
 })
