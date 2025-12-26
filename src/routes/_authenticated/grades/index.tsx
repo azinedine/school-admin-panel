471b180f3d8473b434801c94ffff41221b5937bb
azineddine <amarichezineddine@gmail.com>
2025-12-26 18:42:14 +0100

    feat: Remove Grades page from authenticated admin routes, cleaning up unused components and routes.

--- src/routes/_authenticated/grades/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import GradesPage from '@/pages/GradesPage'
-
-export const Route = createFileRoute('/_authenticated/grades/')({
-  component: GradesPage,
-})
