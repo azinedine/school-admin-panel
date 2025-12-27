c7f1e6ffeade4b399cadc87ed02afa8098f89ccb
azineddine <amarichezineddine@gmail.com>
2025-12-27 14:52:40 +0100

    feat: add barrel export for profile components

---
+++ src/components/profile/index.ts
@@ -0,0 +1,5 @@
+// Profile Components - Barrel Export
+export { ProfileSection } from './ProfileSection'
+export { ProfileInfoRow, ProfileInfoRowList } from './ProfileInfoRow'
+export { ProfileAvatar } from './ProfileAvatar'
+export { ProfileSkeleton } from './ProfileSkeleton'
