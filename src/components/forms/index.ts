6f1ebb8f508f5ac24518121f46f6be760d5ba44b
azineddine <amarichezineddine@gmail.com>
2025-12-27 15:42:39 +0100

    feat: Implement teacher registration fields with new form components and dedicated creation hook.

---
+++ src/components/forms/index.ts
@@ -0,0 +1,5 @@
+// Reusable Form Components
+export { FormSection } from './FormSection'
+export { TextField, TextAreaField } from './TextField'
+export { SelectField } from './SelectField'
+export { MultiSelectField } from './MultiSelectField'
