6074019b993ffad024ec2239455c1166463879b7
azineddine <amarichezineddine@gmail.com>
2025-12-31 10:38:35 +0100

    feat: Create index file for lessons components to streamline exports

---
+++ src/components/features/lessons/index.ts
@@ -0,0 +1,24 @@
+// Tabs
+export { LessonsTab } from './LessonsTab'
+export { PreparationTab } from './PreparationTab'
+export { TimetableTab } from './TimetableTab'
+
+// Lesson Components
+export { LessonForm } from './LessonForm'
+export { LessonCard, LessonCardGrid } from './LessonCard'
+export { LessonViewDialog } from './LessonViewDialog'
+export { LessonPrepForm } from './LessonPrepForm'
+export { LessonPrepDetails } from './LessonPrepDetails'
+export { PreparationCard } from './PreparationCard'
+
+// Form Sub-components
+export { LessonPrepPedagogicalContext } from './LessonPrepPedagogicalContext'
+export { LessonPrepElements } from './LessonPrepElements'
+export { LessonPrepEvaluation } from './LessonPrepEvaluation'
+
+// Utilities
+export { ClassContextDisplay } from './ClassContextDisplay'
+export { LessonDetailDialog } from './LessonDetailDialog'
+export { LessonPrepByClass } from './LessonPrepByClass'
+export { LessonSelector } from './LessonSelector'
+export { LessonDetailsSheet } from './LessonDetailsSheet'
