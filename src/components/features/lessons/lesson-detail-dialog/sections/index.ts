9effcab8aabb330069a2e401b83419a496fb271a
azineddine <amarichezineddine@gmail.com>
2026-01-02 23:27:25 +0100

    feat: Re-export lesson detail sections for improved modularity and organization

---
+++ src/components/features/lessons/lesson-detail-dialog/sections/index.ts
@@ -0,0 +1,5 @@
+// Re-export all sections
+export { BasicInfoSection } from './BasicInfoSection.tsx'
+export { SchedulingSection } from './SchedulingSection.tsx'
+export { StructureSection } from './StructureSection.tsx'
+export { ElementsSection } from './ElementsSection.tsx'
