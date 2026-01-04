d68b7cd71239021f3a87b0670f73ba08b33f2643
azineddine <amarichezineddine@gmail.com>
2026-01-04 19:04:03 +0100

    feat: Implement student management dialogs, attendance tracking, and grade calculation utilities for the grades module.

---
+++ src/components/grades/index.ts
@@ -0,0 +1,13 @@
+// Components
+export { GradeSheetTable } from './GradeSheetTable'
+export { ClassNavigationTabs } from './ClassNavigationTabs'
+export { GradeStatistics } from './GradeStatistics'
+
+// Types
+export * from './types'
+
+// Utils
+export * from './utils/grade-calculations'
+
+// Dialogs
+export * from './dialogs'
