47e3b7b254bb5c92da61594132a38a1574bfd32c
azineddine <amarichezineddine@gmail.com>
2026-01-05 22:32:17 +0100

    feat: create index file for lesson prep elements to streamline component exports

---
+++ src/components/features/lessons/lesson-prep-elements/index.ts
@@ -0,0 +1,4 @@
+export { LessonPrepElements } from './LessonPrepElements'
+export { ElementsHeader } from './ElementsHeader'
+export { ElementsEmptyState } from './ElementsEmptyState'
+export { ElementsItem } from './ElementsItem'
