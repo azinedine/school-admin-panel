e3b29afbc0cbb85a76d692c7683ea067a19876f4
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:56:51 +0100

    refactor: remove LessonPrepPedagogicalContext and related exports to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/lesson-prep-context/index.ts
+++
@@ -1,4 +0,0 @@
-export { LessonPrepPedagogicalContext } from './LessonPrepPedagogicalContext'
-export { ContextDomainField } from './ContextDomainField'
-export { ContextLearningUnitField } from './ContextLearningUnitField'
-export { ContextKnowledgeResourceField } from './ContextKnowledgeResourceField'
