2a65915a91d09b6bb358f2c9a4bf0f627375fba9
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:56:08 +0100

    refactor: remove LessonPrepByClass module to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/lesson-prep-by-class/index.ts
+++
@@ -1,8 +0,0 @@
-// LessonPrepByClass Module - SOLID Architecture
-// Single Responsibility: Table, Dialogs separated
-// Dependency Inversion: All state/logic in use-lesson-prep-by-class hook
-
-export { LessonPrepByClass } from './LessonPrepByClass.tsx'
-export { useLessonPrepByClass } from './use-lesson-prep-by-class.ts'
-export { LessonsTable } from './LessonsTable.tsx'
-export { StatusNoteDialog } from './StatusNoteDialog.tsx'
