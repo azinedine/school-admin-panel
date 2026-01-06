0bdaa4b31dfb7e192d5684c57212c1e2afadc17b
azineddine <amarichezineddine@gmail.com>
2026-01-06 21:11:57 +0100

    refactor: remove PreparationTab module and related exports to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/preparation-tab/index.ts
+++
@@ -1,8 +0,0 @@
-// PreparationTab Module - SOLID Architecture
-// Single Responsibility: Filters, Grid, and Dialogs separated
-// Dependency Inversion: All state/logic in use-preparation-tab hook
-
-export { PreparationTab } from './PreparationTab.tsx'
-export { usePreparationTab, type StatusFilter } from './use-preparation-tab.ts'
-export { PrepFilters } from './PrepFilters.tsx'
-export { PrepGrid } from './PrepGrid.tsx'
