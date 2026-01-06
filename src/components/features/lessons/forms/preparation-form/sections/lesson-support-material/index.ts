c67446c9528dbb12d1c5425ad2761aca2e4fa727
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:47:10 +0100

    feat: add exports for lesson support material components in lesson preparation form, enhancing modularity by consolidating support material-related exports

---
+++ src/components/features/lessons/forms/preparation-form/sections/lesson-support-material/index.ts
@@ -0,0 +1,4 @@
+export { LessonSupportMaterial } from './LessonSupportMaterial'
+export { SupportMaterialsSelect } from './SupportMaterialsSelect'
+export { SupportReferencesSelect } from './SupportReferencesSelect'
+export { SupportKnowledgeList } from './SupportKnowledgeList'
