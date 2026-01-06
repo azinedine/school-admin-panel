9b0cef534984172d055c908af109f6b03352c3cd
azineddine <amarichezineddine@gmail.com>
2026-01-06 21:12:22 +0100

    feat: add shared components for lessons including DynamicList, ClassContextDisplay, LessonPrepByClass, and LessonSelector

---
+++ src/components/features/lessons/shared/index.ts
@@ -0,0 +1,5 @@
+// Shared Components - Common Utilities and Reusable Components
+export { DynamicList } from './components/dynamic-list'
+export { ClassContextDisplay } from './components/class-context-display'
+export { LessonPrepByClass } from './components/lesson-prep-by-class'
+export { LessonSelector } from './components/lesson-selector'
