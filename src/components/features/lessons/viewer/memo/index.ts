933caaec6c699b31e6a6d8528689d4375cb4cdf5
azineddine <amarichezineddine@gmail.com>
2026-01-06 21:21:42 +0100

    feat: add Memo components for read-only lesson preparation view

---
+++ src/components/features/lessons/viewer/memo/index.ts
@@ -0,0 +1,9 @@
+// Memo Components - Read-only lesson preparation view
+export { LessonMemoView } from './LessonMemoView'
+export { MemoHeader } from './MemoHeader'
+export { MemoContext } from './MemoContext'
+export { MemoCoreInfo } from './MemoCoreInfo'
+export { MemoPhases } from './MemoPhases'
+export { MemoEvaluation } from './MemoEvaluation'
+export { MemoResources } from './MemoResources'
+export { MemoSection } from './MemoSection'
