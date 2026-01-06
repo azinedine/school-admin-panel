3dbad5093ceb9e0cb891736bd441f26078625728
azineddine <amarichezineddine@gmail.com>
2026-01-06 21:15:07 +0100

    feat: add tab components for main navigation views in lessons section

---
+++ src/components/features/lessons/tabs/index.ts
@@ -0,0 +1,4 @@
+// Tab Components - Main Navigation Views
+export { LessonsTab } from './lessons-tab'
+export { PreparationTab } from './preparation-tab'
+export { TimetableTab } from './timetable-tab'
