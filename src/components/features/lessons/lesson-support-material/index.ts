2851689387f4b402f969dc44218eb94e1dcd0f0e
azineddine <amarichezineddine@gmail.com>
2026-01-06 12:00:55 +0100

    refactor: remove LessonSupportMaterial exports to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/lesson-support-material/index.ts
+++
@@ -1,4 +0,0 @@
-export { LessonSupportMaterial } from './LessonSupportMaterial'
-export { SupportMaterialsSelect } from './SupportMaterialsSelect'
-export { SupportReferencesSelect } from './SupportReferencesSelect'
-export { SupportKnowledgeList } from './SupportKnowledgeList'
