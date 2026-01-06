b0cec756462e60ad3b7746336c53562633808357
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:42:21 +0100

    feat: add form components for lesson data entry and editing

---
+++ src/components/features/lessons/forms/index.ts
@@ -0,0 +1,9 @@
+// Form Components - Data Entry and Editing
+export { LessonForm } from './lesson-form'
+export { PhaseEditor } from './phase-editor'
+
+// Preparation Form (main + sections)
+export { LessonPrepForm } from './preparation-form/main'
+export { LessonPrepPedagogicalContext } from './preparation-form/sections/lesson-prep-context'
+export { LessonPrepElements } from './preparation-form/sections/lesson-prep-elements'
+export { LessonPrepEvaluation } from './preparation-form/sections/lesson-prep-evaluation'
