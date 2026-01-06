36fda51c0d5ede04341744339e1e53f8f0ef6c23
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:59:39 +0100

    refactor: remove LessonPrepNotes exports to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/lesson-prep-notes/index.ts
+++
@@ -1,4 +0,0 @@
-export { LessonPrepNotes } from './LessonPrepNotes'
-export { NotesHeader } from './NotesHeader'
-export { NotesEmptyState } from './NotesEmptyState'
-export { NotesItem } from './NotesItem'
