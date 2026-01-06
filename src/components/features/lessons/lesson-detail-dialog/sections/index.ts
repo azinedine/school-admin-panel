4f278b4f3b706582b4d580d41ad619772cad9c88
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:53:45 +0100

    refactor: remove sections index file to streamline lesson-related functionality and eliminate unused code

--- src/components/features/lessons/lesson-detail-dialog/sections/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all sections
-export { BasicInfoSection } from './BasicInfoSection.tsx'
-export { SchedulingSection } from './SchedulingSection.tsx'
-export { StructureSection } from './StructureSection.tsx'
-export { ElementsSection } from './ElementsSection.tsx'
