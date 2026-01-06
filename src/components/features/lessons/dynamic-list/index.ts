c7f6ec766947b16c6e9734a9ad90c21cec61e9fc
azineddine <amarichezineddine@gmail.com>
2026-01-06 11:42:16 +0100

    refactor: remove exports for DynamicList components as part of code cleanup

--- src/components/features/lessons/dynamic-list/index.ts
+++
@@ -1,4 +0,0 @@
-export { DynamicList } from './DynamicList'
-export { DynamicListHeader } from './DynamicListHeader'
-export { DynamicListEmptyState } from './DynamicListEmptyState'
-export { DynamicListItem } from './DynamicListItem'
