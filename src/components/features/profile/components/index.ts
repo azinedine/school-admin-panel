8ec59c8c754b5feb0680b22163557172ca1ee651
azineddine <amarichezineddine@gmail.com>
2026-01-07 21:16:19 +0100

    feat: add index file for profile display components to simplify exports

---
+++ src/components/features/profile/components/index.ts
@@ -0,0 +1,5 @@
+// Profile Display Components
+export { ProfileSection } from './ProfileSection'
+export { ProfileInfoRow, ProfileInfoRowList } from './ProfileInfoRow'
+export { ProfileAvatar } from './ProfileAvatar'
+export { ProfileSkeleton } from './ProfileSkeleton'
