ad288d8dfd666d9d4913f938156454b3ce18b6bd
azineddine <amarichezineddine@gmail.com>
2026-01-07 20:44:13 +0100

    refactor: reorganize grade components into a table directory and consolidate shared types and utils

--- src/components/grades/index.ts
+++ src/components/grades/index.ts
@@ -1,13 +1,11 @@
-// Components
-export { GradeSheetTable } from './GradeSheetTable'
-export { ClassNavigationTabs } from './ClassNavigationTabs'
-export { GradeStatistics } from './GradeStatistics'
-
-// Types
-export * from './types'
-
-// Utils
-export * from './utils/grade-calculations'
+// Table Components
+export { GradeSheetTable } from './table/GradeSheetTable'
+export { ClassNavigationTabs } from './table/ClassNavigationTabs'
+export { GradeStatistics } from './table/GradeStatistics'
 
 // Dialogs
 export * from './dialogs'
+
+// Shared (Types & Utils)
+export * from './shared'
+
