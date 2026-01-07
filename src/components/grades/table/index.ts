32165b737e5d97b280e2e2ed6afc5634e5e833a6
azineddine <amarichezineddine@gmail.com>
2026-01-07 20:46:03 +0100

    feat: create index file for grade table components, exporting GradeSheetTable, ClassNavigationTabs, and GradeStatistics

---
+++ src/components/grades/table/index.ts
@@ -0,0 +1,4 @@
+// Table Components - Grade Sheet and Navigation
+export { GradeSheetTable } from './GradeSheetTable'
+export { ClassNavigationTabs } from './ClassNavigationTabs'
+export { GradeStatistics } from './GradeStatistics'
