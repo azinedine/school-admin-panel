182487be96b31d7dd794b5192cf0112d709cdccb
azineddine <amarichezineddine@gmail.com>
2026-01-07 21:11:33 +0100

    feat: create index file for reusable form field components to simplify imports

---
+++ src/components/ui/form-fields/index.ts
@@ -0,0 +1,8 @@
+// Reusable Form Field Components
+export { ActionButton } from './action-button'
+export { DatePicker } from './date-picker'
+export { FormSection } from './form-section'
+export { MultiSelectField } from './multi-select-field'
+export { PasswordField } from './password-field'
+export { SelectField } from './select-field'
+export { TextField, TextAreaField } from './text-field'
