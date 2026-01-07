6928b420b95705194ff8897e5a2d068399bb5b65
azineddine <amarichezineddine@gmail.com>
2026-01-07 21:09:14 +0100

    refactor: remove index.ts file for form components to streamline codebase

--- src/components/forms/index.ts
+++
@@ -1,7 +0,0 @@
-// Reusable Form Components
-export { FormSection } from './FormSection'
-export { TextField, TextAreaField } from './TextField'
-export { SelectField } from './SelectField'
-export { MultiSelectField } from './MultiSelectField'
-export { DatePicker } from './DatePicker'
-export { ActionButton } from './ActionButton'
