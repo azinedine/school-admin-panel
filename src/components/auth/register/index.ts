f2dfae298ee90e49e51358b5aae0e6d862fbff48
azineddine <amarichezineddine@gmail.com>
2026-01-07 20:57:53 +0100

    feat: create index file for registration form sections, exporting AccountInfo, Location, PersonalInfo, ProfessionalInfo, Academic, and AdminAdditionalInfo sections

---
+++ src/components/auth/register/index.ts
@@ -0,0 +1,7 @@
+// Registration Form Sections
+export { AccountInfoSection } from './AccountInfoSection'
+export { LocationSection } from './LocationSection'
+export { PersonalInfoSection } from './PersonalInfoSection'
+export { ProfessionalInfoSection } from './ProfessionalInfoSection'
+export { AcademicSection } from './AcademicSection'
+export { AdminAdditionalInfoSection } from './AdminAdditionalInfoSection'
