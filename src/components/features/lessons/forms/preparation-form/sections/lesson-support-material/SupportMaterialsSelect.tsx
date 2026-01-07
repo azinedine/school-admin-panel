675cc67d64e6b8e2d7a67093413309174a3d3dd6
azineddine <amarichezineddine@gmail.com>
2026-01-07 21:08:55 +0100

    refactor: update import path for MultiSelectField in SupportMaterialsSelect component to align with new directory structure

--- src/components/features/lessons/forms/preparation-form/sections/lesson-support-material/SupportMaterialsSelect.tsx
+++ src/components/features/lessons/forms/preparation-form/sections/lesson-support-material/SupportMaterialsSelect.tsx
@@ -2,7 +2,7 @@ import { type Control, Controller } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import type { LessonPreparationFormData } from '@/schemas/lesson-preparation'
 import { useMaterials } from '@/hooks/use-materials'
-import { MultiSelectField } from '@/components/forms/MultiSelectField'
+import { MultiSelectField } from '@/components/ui/form-fields'
 
 interface SupportMaterialsSelectProps {
     control: Control<LessonPreparationFormData>
