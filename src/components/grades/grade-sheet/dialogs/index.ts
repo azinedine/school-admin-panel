60c93d28123b2c3630b071443963529d5b8c1074
azineddine <amarichezineddine@gmail.com>
2026-01-09 17:24:14 +0100

    feat: add barrel exports for dialog components in grade sheet to simplify imports

---
+++ src/components/grades/grade-sheet/dialogs/index.ts
@@ -0,0 +1,6 @@
+// Barrel exports for dialog components
+export { AttendanceDialog } from './AttendanceDialog'
+export { AttendanceHistoryDialog } from './AttendanceHistoryDialog'
+export { MoveStudentDialog } from './MoveStudentDialog'
+export { RemoveStudentDialog } from './RemoveStudentDialog'
+export { AddStudentDialog } from './AddStudentDialog'
